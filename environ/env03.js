ac4882421b4a8308109338ca627cd64bde26f75c
hferrill <hferrill05@gmail.com>
2026-05-18 03:13:04 -0700

    gfd
    ghfd

--- environ/env03.js
+++ environ/env03.js
@@ -1,49 +1,19 @@
-
-
-let environmentTitle = "that damn creek";
-
-let environmentElements = ["sticky mud", "slimy rocks", "stagnant water", "gnarly tree roots"];
-
-
-
-let count = 0; 
-
-
-$("#crAdd").click(  function(){
-
-    count = count +1;
-    $("#crAdd").html("yes papa " + count + " times");
-
- let crName = $("#crName").val();
-    let crColor = $("#crColor").val();
-    let crEyesNum = $("#crEyesNum").val();
-
-    // lets construct html for eyes
-     let crEyesHtml="";
-    for (let i = 0; i < crEyesNum; i++) {
-        crEyesHtml=crEyesHtml+ "<div class='eye'>.</div>";
-    }
-  $( "crAdd" ).click(function() {
-    $( "crAdd" ).css('background', 'green');
+$(document).ready(function(){
+  $("button").click(function(){
+    $("p").replaceWith(function(n){
+      return "<h3> FOOLED, this is a LAKE " + n + ".</h3>";
+    });
   });
+});
 
-    // test in console
-    console.log(crName, crColor, crEyesNum);
+$(document).ready(function(){
+  $("p").click(function(){
+    $(this).hide();
+  });
+});
 
-
-    //if vlaues
-    if (crName.length > 3) {
-
-       $("#creature-list").append(`
-            <div class="creature">
-                <div class="creature-body" style="background-color: ${crColor}"> ${crEyesHtml} ${crMood} </div>
-                <div class="creature-info">${crName}</div>
-            </div>sad
-        `);
-
-
- $("#environment-output").html(message);
-
-  console.log(count);
-  
+$(document).ready(function(){
+  $("img").click(function(){
+    $(this).hide();
+  });
 });
